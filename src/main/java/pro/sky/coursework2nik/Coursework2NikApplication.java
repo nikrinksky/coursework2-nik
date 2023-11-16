4d56280248a18696fbbd5f0834226cb71fdf5575
Nik480_1 <nik_rink@mail.com>
2023-11-16 22:32:35 +0300

    Комит для курсовой_2 в master ветку

---
+++ src/main/java/pro/sky/coursework2nik/Coursework2NikApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2nik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2NikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework2NikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/coursework2nik/Coursework2NikApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2nik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2NikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
